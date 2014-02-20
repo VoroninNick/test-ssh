3f35a388c83a9a830c75ccfc36559a0d4cd3c711
pkorenev <pasha_kor@mail.ru>
2014-02-20 08:48:01 +0200

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestSsh::Application.config.secret_key_base = '97a9d8428ba8375f9f24e0c43147cc1e992ce48e3fec2b5c2e7aecfbee1af4d14bc02b5a2da5bd30c0a53e09a3f3f17628155d6997af3a1a7110f7860b17c192'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestSsh::Application.config.session_store :cookie_store, key: '_test-ssh_session'
